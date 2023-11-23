e976534de499e4078d9fd3a1886e3c6a0ee0e928
Omar Franco <fofe2803@gmail.com>
2023-11-23 01:57:33 -0600

    feat(user): create and migrate the user model

--- traceabilitymatrix/user/models.py
+++ traceabilitymatrix/user/models.py
@@ -1,3 +1,10 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    full_name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=255)
+    is_admin = models.BooleanField(default=False)
+    is_guest = models.BooleanField(default=False)
+    assigned_projects = models.ManyToManyField('project.Project', related_name='%(class)s_assigned_users', blank=True)
