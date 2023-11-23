8ea90b33aab32dac0edca8cefd2c2889cef45e3b
Omar Franco <fofe2803@gmail.com>
2023-11-23 01:53:53 -0600

    feat(traceabilitymatrix): config to use my custom user
    add config to use my custom user as the default model

--- traceabilitymatrix/traceabilitymatrix/settings.py
+++ traceabilitymatrix/traceabilitymatrix/settings.py
@@ -47,9 +47,12 @@ INSTALLED_APPS = [
     "rest_framework",
     "dj_rest_auth",
     "rest_framework.authtoken",
+    "user",
+    "project",
+    "matrix",
+    "record",
 ]
 
-TOKEN_MODEL = None
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
@@ -125,7 +128,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "America/Mexico_City"
 
 USE_I18N = True
 
@@ -141,3 +144,8 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+AUTH_USER_MODEL = 'user.User'
