1fd178ab5dbf71f1af0d58bc665137925d1bf7c0
Omar Franco <fofe2803@gmail.com>
2023-11-23 01:57:55 -0600

    feat(record): create and migrate the record model

--- traceabilitymatrix/record/models.py
+++ traceabilitymatrix/record/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from matrix.models import Matrix
 
-# Create your models here.
+class Record(models.Model):
+    associated_matrix = models.ForeignKey(Matrix, on_delete=models.CASCADE, related_name='associated_records')
+    custom_project_id = models.CharField(max_length=255)
+    sprint = models.CharField(max_length=255)
+    artifact_name = models.CharField(max_length=255)
+    url_resource = models.URLField(null=True, blank=True)
+    location_resource = models.FileField(upload_to='uploads/', null=True, blank=True)
+    key_relationship = models.CharField(max_length=255)
+    status = models.CharField(max_length=255)
+    responsible = models.CharField(max_length=255)
+    creation_date = models.DateField(auto_now_add=True)
+    update_date = models.DateField(auto_now=True)
+    impact = models.CharField(max_length=255)
+    notes = models.TextField(blank=True)
