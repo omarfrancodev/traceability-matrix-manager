c80d4f2d3cf5cc80a3c2598565a80bf23275fea8
Omar Franco <fofe2803@gmail.com>
2023-11-23 01:58:27 -0600

    feat(project): create and migrate the project model

--- traceabilitymatrix/project/models.py
+++ traceabilitymatrix/project/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Project(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    assigned_users = models.ManyToManyField('user.User', related_name='%(class)s_assigned_projects', blank=True)
+    associated_matrix = models.OneToOneField('matrix.Matrix', on_delete=models.CASCADE, related_name='%(class)s_associated_project', blank=True)
