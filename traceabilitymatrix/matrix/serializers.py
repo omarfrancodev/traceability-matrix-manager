9c47b1bc3151bc72fc10ef7563c22021f6085b78
Omar Franco <fofe2803@gmail.com>
2023-11-24 13:29:17 -0600

    feat(matrix): creation of the serializers for matrix

---
+++ traceabilitymatrix/matrix/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from .models import Matrix
+
+class MatrixSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Matrix
+        fields = ['id', 'associated_project']
