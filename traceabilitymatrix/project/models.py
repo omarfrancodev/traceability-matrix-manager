70f49a2d2b542b92ada66583f6942177ef067d64
Omar Franco <fofe2803@gmail.com>
2023-11-24 13:25:18 -0600

    refactor(project): rename the relationships with other models and migrate

--- traceabilitymatrix/project/models.py
+++ traceabilitymatrix/project/models.py
@@ -3,5 +3,5 @@ from django.db import models
 class Project(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
-    assigned_users = models.ManyToManyField('user.User', related_name='%(class)s_assigned_projects', blank=True)
-    associated_matrix = models.OneToOneField('matrix.Matrix', on_delete=models.CASCADE, related_name='%(class)s_associated_project', blank=True)
+    assignedUsers = models.ManyToManyField('user.User', related_name='%(class)s_assignedProjects', blank=True)
+    associatedMatrix = models.OneToOneField('matrix.Matrix', on_delete=models.CASCADE, related_name='%(class)s_associatedProject', blank=True)
