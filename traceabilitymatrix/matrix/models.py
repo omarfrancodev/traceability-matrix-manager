5ab2388c14821d17abd9f287fd62955bfbd4152f
Omar Franco <fofe2803@gmail.com>
2023-11-24 13:28:47 -0600

    refactor(matrix): rename some attributes and relationship with others models, and migrate

--- traceabilitymatrix/matrix/models.py
+++ traceabilitymatrix/matrix/models.py
@@ -2,4 +2,4 @@ from django.db import models
 from project.models import Project
 
 class Matrix(models.Model):
-    associated_project = models.OneToOneField(Project, on_delete=models.CASCADE, related_name='%(class)s_associated_matrix')
+    associatedProject = models.OneToOneField(Project, on_delete=models.CASCADE, related_name='%(class)s_associatedMatrix')
