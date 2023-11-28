b0cdf67e3dc589cb977c5f7746abfcba45fa735f
Omar Franco <fofe2803@gmail.com>
2023-11-28 13:29:46 -0600

    refactor: apply migrations and refactor url path names

--- traceabilitymatrix/record/urls.py
+++ traceabilitymatrix/record/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import RecordListView, RecordDetailView
 
 urlpatterns = [
-    path('/', RecordListView.as_view(), name='record-list'),
-    path('/<int:pk>/', RecordDetailView.as_view(), name='record-detail'),
+    path('', RecordListView.as_view(), name='record-list'),
+    path('<int:pk>', RecordDetailView.as_view(), name='record-detail'),
 ]
