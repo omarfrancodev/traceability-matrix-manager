33d1ecefd05593a6fdd922480cac5d8d6e58a16a
Omar Franco <fofe2803@gmail.com>
2023-11-28 10:53:17 -0600

    feat(record): add urls paths to record application

---
+++ traceabilitymatrix/record/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import RecordListView, RecordDetailView
+
+urlpatterns = [
+    path('/', RecordListView.as_view(), name='record-list'),
+    path('/<int:pk>/', RecordDetailView.as_view(), name='record-detail'),
+]
