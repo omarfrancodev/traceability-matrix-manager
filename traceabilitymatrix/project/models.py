48c527d6ec24c2c5a48dc8eae64cc61e690995bd
Omar Franco <fofe2803@gmail.com>
2023-11-28 13:24:20 -0600

    refactor(project): add nullable true to associatedMatrix field

--- traceabilitymatrix/project/models.py
+++ traceabilitymatrix/project/models.py
@@ -4,4 +4,4 @@ class Project(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
     assignedUsers = models.ManyToManyField('user.User', related_name='%(class)s_assignedProjects', blank=True)
-    associatedMatrix = models.OneToOneField('matrix.Matrix', on_delete=models.CASCADE, related_name='%(class)s_associatedProject', blank=True)
+    associatedMatrix = models.OneToOneField('matrix.Matrix', on_delete=models.CASCADE, related_name='%(class)s_associatedProject', null=True, blank=True)
