60cde634069194c1451b54a040d8298ce14ae340
Omar Franco <fofe2803@gmail.com>
2023-11-28 10:35:14 -0600

    feat(record): creation of the views (full CRUD) of the record app

--- traceabilitymatrix/record/views.py
+++ traceabilitymatrix/record/views.py
@@ -1,3 +1,85 @@
-from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.response import Response
+from .models import Record
+from .serializers import RecordSerializer
 
-# Create your views here.
+class RecordListView(generics.ListCreateAPIView):
+    queryset = Record.objects.all()
+    serializer_class = RecordSerializer
+
+    def list(self, request, *args, **kwargs):
+        try:
+            response = super().list(request, *args, **kwargs)
+            return Response(
+                data={'message': 'Records retrieved successfully', 'recordData': response.data},
+                status=status.HTTP_200_OK
+            )
+        except Exception as e:
+            return Response(
+                data={'message': f'Error retrieving records: {str(e)}'},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
+
+    def create(self, request, *args, **kwargs):
+        try:
+            serializer = self.get_serializer(data=request.data)
+            serializer.is_valid(raise_exception=True)
+            self.perform_create(serializer)
+            headers = self.get_success_headers(serializer.data)
+            return Response(
+                data={'message': 'Record created successfully', 'recordData': serializer.data},
+                status=status.HTTP_201_CREATED,
+                headers=headers
+            )
+        except Exception as e:
+            return Response(
+                data={'message': f'Error creating record: {str(e)}'},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+class RecordDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Record.objects.all()
+    serializer_class = RecordSerializer
+
+    def retrieve(self, request, *args, **kwargs):
+        try:
+            response = super().retrieve(request, *args, **kwargs)
+            return Response(
+                data={'message': 'Record retrieved successfully', 'recordData': response.data},
+                status=status.HTTP_200_OK
+            )
+        except Exception as e:
+            return Response(
+                data={'message': f'Error retrieving record: {str(e)}'},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+    def update(self, request, *args, **kwargs):
+        try:
+            instance = self.get_object()
+            serializer = self.get_serializer(instance, data=request.data, partial=True)
+            serializer.is_valid(raise_exception=True)
+            self.perform_update(serializer)
+            return Response(
+                data={'message': 'Record updated successfully', 'recordData': serializer.data},
+                status=status.HTTP_200_OK
+            )
+        except Exception as e:
+            return Response(
+                data={'message': f'Error updating record: {str(e)}'},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+    def destroy(self, request, *args, **kwargs):
+        try:
+            instance = self.get_object()
+            self.perform_destroy(instance)
+            return Response(
+                data={'message': 'Record deleted successfully'},
+                status=status.HTTP_204_NO_CONTENT
+            )
+        except Exception as e:
+            return Response(
+                data={'message': f'Error deleting record: {str(e)}'},
+                status=status.HTTP_400_BAD_REQUEST
+            )
