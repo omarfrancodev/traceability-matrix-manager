3623e1e6cb423ebd9189b51e8144ff8fe8d45863
Omar Franco <fofe2803@gmail.com>
2023-11-30 13:00:04 -0600

    feat(record): add a validation of the keyRelationship value
    add a validation of the keyRelationship value when a record will be save or update

--- traceabilitymatrix/record/signals.py
+++ traceabilitymatrix/record/signals.py
@@ -1,6 +1,9 @@
 from django.db.models.signals import post_delete
+from django.db.models.signals import pre_save
 from django.dispatch import receiver
+from django.core.exceptions import ValidationError
 from .models import Record
+from user.models import User
 import os
         
 @receiver(post_delete, sender=Record)
@@ -8,3 +11,16 @@ def delete_record_file(sender, instance, **kwargs):
     if instance.locationResource:
         if os.path.isfile(instance.locationResource.path):
             os.remove(instance.locationResource.path)
+
+@receiver(pre_save, sender=Record)
+def validate_key_relationship(sender, instance, **kwargs):
+    if instance.keyRelationship == "NA":
+        return
+
+    existingRecords = Record.objects.filter(
+        associatedProject=instance.associatedProject
+    ).exclude(pk=instance.pk)
+    matchingCustomProjectIds = existingRecords.values_list('projectRecordId', flat=True)
+    
+    if instance.keyRelationship not in matchingCustomProjectIds:
+        raise ValidationError('El valor de keyRelationship debe ser igual a uno de los projectRecordId existentes o "NA"')
