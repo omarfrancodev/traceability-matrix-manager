c38b0f187c8d4fa0a46b578652bba015fbb48028
Omar Franco <fofe2803@gmail.com>
2023-11-30 19:16:44 -0600

    feat(record): move a logic to generate a custom id for record and reorder some signals
    move the logic to generate the projectRecordId to a signal, reorder the signals and change the post_delete signal due to the changes on the model

--- traceabilitymatrix/record/signals.py
+++ traceabilitymatrix/record/signals.py
@@ -1,16 +1,26 @@
-from django.db.models.signals import post_delete
-from django.db.models.signals import pre_save
+from django.db.models.signals import pre_save, post_save, post_delete
 from django.dispatch import receiver
 from django.core.exceptions import ValidationError
 from .models import Record
-from user.models import User
 import os
-        
-@receiver(post_delete, sender=Record)
-def delete_record_file(sender, instance, **kwargs):
-    if instance.locationResource:
-        if os.path.isfile(instance.locationResource.path):
-            os.remove(instance.locationResource.path)
+
+
+@receiver(pre_save, sender=Record)
+def generate_project_record_id(sender, instance, **kwargs):
+    associatedProject = instance.associatedProject
+    projectName = associatedProject.name
+    projectNameAbbr = "".join([word[0].upper() for word in projectName.split()])
+
+    existing_records_count = Record.objects.filter(
+        associatedProject=associatedProject, sprint=instance.sprint
+    ).count()
+
+    customProjectRecordId = (
+        f"{projectNameAbbr}-S-{instance.sprint}-{existing_records_count + 1:03d}"
+    )
+
+    instance.projectRecordId = customProjectRecordId
+
 
 @receiver(pre_save, sender=Record)
 def validate_key_relationship(sender, instance, **kwargs):
@@ -20,7 +30,18 @@ def validate_key_relationship(sender, instance, **kwargs):
     existingRecords = Record.objects.filter(
         associatedProject=instance.associatedProject
     ).exclude(pk=instance.pk)
-    matchingCustomProjectIds = existingRecords.values_list('projectRecordId', flat=True)
-    
+    matchingCustomProjectIds = existingRecords.values_list("projectRecordId", flat=True)
+
     if instance.keyRelationship not in matchingCustomProjectIds:
-        raise ValidationError('El valor de keyRelationship debe ser igual a uno de los projectRecordId existentes o "NA"')
+        raise ValidationError(
+            'El valor de keyRelationship debe ser igual a uno de los projectRecordId existentes o "NA"'
+        )
+
+
+@receiver(post_delete, sender=Record)
+def delete_record_file(sender, instance, **kwargs):
+    resources = instance.files.all()
+
+    for resource in resources:
+        if resource.file and os.path.isfile(resource.file.path):
+            os.remove(resource.file.path)
