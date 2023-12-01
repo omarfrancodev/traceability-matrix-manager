5dc1b24ea84e896f8c6385e355803f3b94f38909
Omar Franco <fofe2803@gmail.com>
2023-11-30 19:13:29 -0600

    feat(record): add internal models and migrates the changes
    add internal models realates to record model to be able to upload multiple files and urls, remove the fields on the record model, and applies the migration to db

--- traceabilitymatrix/record/models.py
+++ traceabilitymatrix/record/models.py
@@ -1,17 +1,42 @@
 from django.db import models
 from project.models import Project
+from django_currentuser.db.models import CurrentUserField
+
 
 class Record(models.Model):
-    associatedProject = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='associatedRecords')
-    customProjectId = models.CharField(max_length=255, blank=True)
+    associatedProject = models.ForeignKey(
+        Project, on_delete=models.CASCADE, related_name="associatedRecords"
+    )
+    projectRecordId = models.CharField(max_length=255, blank=True)
     sprint = models.CharField(max_length=255)
     artifactName = models.CharField(max_length=255)
-    urlResource = models.URLField(null=True, blank=True)
-    locationResource = models.FileField(upload_to='files/', null=True, blank=True)
     keyRelationship = models.CharField(max_length=255)
     status = models.CharField(max_length=255)
-    responsible = models.CharField(max_length=255)
+    createdBy = CurrentUserField(related_name="createdRecordBy")
+    modifiedBy = CurrentUserField(on_update=True, related_name="modifiedRecordsBy")
     creationDate = models.DateField(auto_now_add=True)
     updateDate = models.DateField(auto_now=True)
     impact = models.CharField(max_length=255)
     notes = models.TextField(blank=True)
+
+
+class ResourceURL(models.Model):
+    record = models.ForeignKey(
+        Record,
+        on_delete=models.CASCADE,
+        related_name="urls",
+        blank=True,
+        null=True,
+    )
+    url = models.URLField(null=True, blank=True)
+
+
+class ResourceFile(models.Model):
+    record = models.ForeignKey(
+        Record,
+        on_delete=models.CASCADE,
+        related_name="files",
+        blank=True,
+        null=True,
+    )
+    file = models.FileField(upload_to="files/", null=True, blank=True)
