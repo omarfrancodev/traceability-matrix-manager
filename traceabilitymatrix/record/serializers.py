f1ad04c005cfe772408da85c4accda7dd12a200c
Omar Franco <fofe2803@gmail.com>
2023-12-05 14:26:08 -0600

    feat(record): add logic to update urls and files, rename fields
    add logic to be able to update urls and files due to the main structure of record, rename the date fields due to previous changes on model

--- traceabilitymatrix/record/serializers.py
+++ traceabilitymatrix/record/serializers.py
@@ -36,8 +36,8 @@ class RecordSerializer(serializers.ModelSerializer):
             "status",
             "createdBy",
             "modifiedBy",
-            "creationDate",
-            "updateDate",
+            "createdAt",
+            "updatedAt",
             "impact",
             "notes",
             "files",
@@ -71,3 +71,25 @@ class RecordSerializer(serializers.ModelSerializer):
                 ResourceURL.objects.create(record=record, url=url)
 
         return record
+    
+    def update(self, instance, validated_data):
+        for field in instance._meta.fields:
+            field_name = field.name
+            if field_name in validated_data:
+                setattr(instance, field_name, validated_data[field_name])
+                
+        uploadedFiles = validated_data.get("uploadedFiles", [])
+        self.update_relation(ResourceFile, instance, "file", uploadedFiles)
+
+        uploadedUrls = validated_data.get("uploadedURLs", [])
+        self.update_relation(ResourceURL, instance, "url", uploadedUrls)
+
+        instance.save()
+        return instance
+
+    def update_relation(self, model, instance, field_name, data):
+        model.objects.filter(record=instance).delete()
+
+        for newData in data:
+            if newData:
+                model.objects.create(record=instance, **{field_name: newData})
