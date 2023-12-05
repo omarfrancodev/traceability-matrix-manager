154afa717d9faa1b2b74475b4befc8869cd04442
Omar Franco <fofe2803@gmail.com>
2023-12-05 11:39:36 -0600

    feat(user): change the serialier to show the user projects information and update it

--- traceabilitymatrix/user/serializers.py
+++ traceabilitymatrix/user/serializers.py
@@ -1,7 +1,6 @@
-from .models import User
 from rest_framework import serializers
 from djoser.serializers import UserCreateSerializer, UserSerializer
-
+from project.serializers import ProjectSerializer
 
 class CustomUserCreateSerializer(UserCreateSerializer):
     class Meta(UserCreateSerializer.Meta):
@@ -9,9 +8,20 @@ class CustomUserCreateSerializer(UserCreateSerializer):
 
 
 class CustomUserSerializer(UserSerializer):
-    assignedProjects = serializers.SlugRelatedField(
-        many=True, read_only=True, slug_field="name"
+    projects = ProjectSerializer(many=True, read_only=True)
+    assignedProjects = serializers.ListField(
+        child=serializers.IntegerField(),
+        write_only=True,
+        allow_empty=True,
     )
-
+    
     class Meta(UserSerializer.Meta):
-        fields = ["id", "fullName", "email", "role", "assignedProjects"]
+        fields = ["id", "fullName", "email", "role", "projects", "assignedProjects"]
+    
+    def update(self, instance, validated_data):
+        assignedProjectIds = validated_data.pop("assignedProjects", [])
+        instance = super().update(instance, validated_data)
+        if assignedProjectIds:
+            instance.projects.set(assignedProjectIds)
+        
+        return instance
