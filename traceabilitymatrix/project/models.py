cd2aed1704e5817df8e5aa36dd5ed907303d36a2
Omar Franco <fofe2803@gmail.com>
2023-12-05 14:27:16 -0600

    feat(project): add new fields (isDraft, createdAt, updatedAt and owner), and apply migrations

--- traceabilitymatrix/project/models.py
+++ traceabilitymatrix/project/models.py
@@ -1,5 +1,10 @@
 from django.db import models
+from django_currentuser.db.models import CurrentUserField
 
 class Project(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
+    isDraft = models.BooleanField(default=False)
+    createdAt = models.DateTimeField(auto_now_add=True)
+    updatedAt = models.DateTimeField(auto_now=True)
+    owner = CurrentUserField(related_name="owner")
