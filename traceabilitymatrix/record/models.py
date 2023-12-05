85bc587cd6a086f8c9baa597df6a9bbb5514da72
Omar Franco <fofe2803@gmail.com>
2023-12-04 19:06:48 -0600

    feat(record): add new fields for choices selections and migrate

--- traceabilitymatrix/record/models.py
+++ traceabilitymatrix/record/models.py
@@ -4,19 +4,39 @@ from django_currentuser.db.models import CurrentUserField
 
 
 class Record(models.Model):
+    class Phase(models.TextChoices):
+        Approval = "Approval", "Approval"
+        Initiation = "Initiation", "Initiation"
+        Development = "Development", "Development"
+
+    class Status(models.TextChoices):
+        Proposed = "Proposed", "Proposed"
+        Implemented = "Implemented", "Implemented"
+        Verifying = "Verifying", "Verifying"
+        Approved = "Approved", "Approved"
+
+    class Impact(models.TextChoices):
+        NA = "NA", "NA"
+        Low = "Low", "Low"
+        Medium = "Medium", "Medium"
+        High = "High", "High"
+
     associatedProject = models.ForeignKey(
         Project, on_delete=models.CASCADE, related_name="associatedRecords"
     )
     projectRecordId = models.CharField(max_length=255, blank=True)
     sprint = models.CharField(max_length=255)
+    phase = models.CharField(max_length=25, choices=Phase.choices)
+    type = models.CharField(max_length=255)
     artifactName = models.CharField(max_length=255)
+    description = models.CharField(max_length=255, blank=True)
     keyRelationship = models.CharField(max_length=255)
-    status = models.CharField(max_length=255)
+    status = models.CharField(max_length=25, choices=Status.choices)
     createdBy = CurrentUserField(related_name="createdRecordBy")
     modifiedBy = CurrentUserField(on_update=True, related_name="modifiedRecordsBy")
     creationDate = models.DateField(auto_now_add=True)
     updateDate = models.DateField(auto_now=True)
-    impact = models.CharField(max_length=255)
+    impact = models.CharField(max_length=25, choices=Impact.choices)
     notes = models.TextField(blank=True)
 
 
