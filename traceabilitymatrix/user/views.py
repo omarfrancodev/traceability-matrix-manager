9426d327170b83bdbafb77ab4f87d9002b7afe54
Omar Franco <fofe2803@gmail.com>
2023-12-04 18:51:24 -0600

    feat(user): create the views (RUD) of the user app

--- traceabilitymatrix/user/views.py
+++ traceabilitymatrix/user/views.py
@@ -1,3 +1,83 @@
-from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.response import Response
+from .models import User
+from .serializers import CustomUserSerializer
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+class UserListView(generics.ListAPIView):
+    queryset = User.objects.all().order_by('id')
+    serializer_class = CustomUserSerializer
+    permission_classes = [IsAuthenticated]
+
+    def list(self, request, *args, **kwargs):
+        try:
+            response = super().list(request, *args, **kwargs)
+            return Response(
+                data={'message': 'Users retrieved successfully', 'usersData': response.data},
+                status=status.HTTP_200_OK
+            )
+        except Exception as e:
+            return Response(
+                data={'message': f'Error retrieving users: {str(e)}'},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
+
+class UserDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = CustomUserSerializer
+    permission_classes = [IsAuthenticated]
+
+    def retrieve(self, request, *args, **kwargs):
+        try:
+            response = super().retrieve(request, *args, **kwargs)
+            return Response(
+                data={'message': 'User retrieved successfully', 'userData': response.data},
+                status=status.HTTP_200_OK
+            )
+        except Exception as e:
+            return Response(
+                data={'message': f'Error retrieving user: {str(e)}'},
+                status=status.HTTP_404_NOT_FOUND
+            )
+
+    def update(self, request, *args, **kwargs):
+        try:
+            try:
+                instance = self.get_object()
+            except Exception as e:
+                return Response(
+                    data={'message': f'Error finding user: {str(e)}'},
+                    status=status.HTTP_404_NOT_FOUND
+                )
+            serializer = self.get_serializer(instance, data=request.data, partial=True)
+            serializer.is_valid(raise_exception=True)
+            self.perform_update(serializer)
+            return Response(
+                data={'message': 'User updated successfully', 'userData': serializer.data},
+                status=status.HTTP_200_OK
+            )
+        except Exception as e:
+            return Response(
+                data={'message': f'Error updating user: {str(e)}'},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+    def destroy(self, request, *args, **kwargs):
+        try:
+            try:
+                instance = self.get_object()
+            except Exception as e:
+                return Response(
+                    data={'message': f'Error finding user: {str(e)}'},
+                    status=status.HTTP_404_NOT_FOUND
+                )
+            self.perform_destroy(instance)
+            return Response(
+                data={'message': 'User deleted successfully'},
+                status=status.HTTP_204_NO_CONTENT
+            )
+        except Exception as e:
+            return Response(
+                data={'message': f'Error deleting user: {str(e)}'},
+                status=status.HTTP_404_NOT_FOUND
+            )
