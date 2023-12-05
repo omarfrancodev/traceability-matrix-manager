95ea4409ae998d653eb63ec4e65e7759363fc577
Omar Franco <fofe2803@gmail.com>
2023-12-05 12:15:56 -0600

    style(project): add a slash to url path :)

--- traceabilitymatrix/project/urls.py
+++ traceabilitymatrix/project/urls.py
@@ -4,5 +4,5 @@ from .views import ProjectListCreateView, ProjectDetailView, ProjectUsersView
 urlpatterns = [
     path('', ProjectListCreateView.as_view(), name='project-list'),
     path('<int:pk>', ProjectDetailView.as_view(), name='project-detail'),
-    path('<int:pk>/users', ProjectUsersView.as_view(), name='project-users'),
+    path('<int:pk>/users/', ProjectUsersView.as_view(), name='project-users'),
 ]
