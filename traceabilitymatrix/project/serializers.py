da7ad0a4c7bc2f9a7485fd40bd05e7c7b5033228
Omar Franco <fofe2803@gmail.com>
2023-12-05 14:29:14 -0600

    feat(project): add new serializer and change others due to the new fields and view logic

--- traceabilitymatrix/project/serializers.py
+++ traceabilitymatrix/project/serializers.py
@@ -3,20 +3,30 @@ from .models import Project
 from record.serializers import RecordSerializer
 
 class ProjectSerializer(serializers.ModelSerializer):
+    owner = serializers.SlugRelatedField(read_only=True, slug_field="fullName")
+    
     class Meta:
         model = Project
-        fields = ['id', 'name', 'description']
+        fields = ['id', 'name', 'description', 'isDraft', 'owner']
         
 class DetailProjectSerializer(serializers.ModelSerializer):
+    owner = serializers.SlugRelatedField(read_only=True, slug_field="fullName")
+    
+    class Meta:
+        model = Project
+        fields = ['id', 'name', 'description', 'owner', 'isDraft', 'createdAt', 'updatedAt']
+
+class DetailProjectRecordSerializer(serializers.ModelSerializer):
     associatedRecords = RecordSerializer(many=True, read_only=True)
 
     class Meta:
         model = Project
-        fields = ['id', 'name', 'description', 'associatedRecords']
+        fields = ['id', 'name', 'associatedRecords']
 
 class DetailUsersProjectSerializer(serializers.ModelSerializer):
     assignedUsers = serializers.SlugRelatedField(many=True, read_only=True, slug_field="fullName")
+    owner = serializers.SlugRelatedField(read_only=True, slug_field="fullName")
 
     class Meta:
         model = Project
-        fields = ['id', 'name', 'assignedUsers']
+        fields = ['id', 'name', 'owner', 'assignedUsers']
