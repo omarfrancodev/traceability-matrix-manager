e48b5d3fe1dcc0506f9c0e3ec57fc7f0aec826c7
Omar Franco <fofe2803@gmail.com>
2023-12-04 18:58:23 -0600

    refactor: remove the useless app authuser due to use djoser package

--- traceabilitymatrix/authuser/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import include, path
-from .views import CustomLoginView, CustomLogoutView
-
-urlpatterns = [
-    # path("", include("dj_rest_auth.urls")),
-    path("login/", CustomLoginView.as_view(), name="login"),
-    path("logout/", CustomLogoutView.as_view(), name="logout"),
-]
