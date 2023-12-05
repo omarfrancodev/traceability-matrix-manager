9fb11f4a94e82a71f3bd0a6d2365eb90541e4308
Omar Franco <fofe2803@gmail.com>
2023-12-05 14:31:48 -0600

    feat(user): add and change logic for get the user projects
    add and change logic for get the user projects on a better presetnation and the not assigned projects

--- traceabilitymatrix/user/serializers.py
+++ traceabilitymatrix/user/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from djoser.serializers import UserCreateSerializer, UserSerializer
 from project.serializers import ProjectSerializer
+from project.models import Project
 
 class CustomUserCreateSerializer(UserCreateSerializer):
     class Meta(UserCreateSerializer.Meta):
@@ -8,20 +9,26 @@ class CustomUserCreateSerializer(UserCreateSerializer):
 
 
 class CustomUserSerializer(UserSerializer):
-    projects = ProjectSerializer(many=True, read_only=True)
+    myProjects = ProjectSerializer(many=True, read_only=True)
     assignedProjects = serializers.ListField(
         child=serializers.IntegerField(),
         write_only=True,
         allow_empty=True,
     )
+    otherProjects = serializers.SerializerMethodField()
     
     class Meta(UserSerializer.Meta):
-        fields = ["id", "fullName", "email", "role", "projects", "assignedProjects"]
+        fields = ["id", "fullName", "email", "role", "myProjects", "otherProjects", "assignedProjects"]
+    
+    def get_otherProjects(self, instance):
+        assignedProjectIds = instance.myProjects.values_list("id", flat=True)
+        other_projects = Project.objects.exclude(id__in=assignedProjectIds)
+        return ProjectSerializer(other_projects, many=True).data
     
     def update(self, instance, validated_data):
         assignedProjectIds = validated_data.pop("assignedProjects", [])
         instance = super().update(instance, validated_data)
         if assignedProjectIds:
-            instance.projects.set(assignedProjectIds)
+            instance.myProjects.set(assignedProjectIds)
         
         return instance
