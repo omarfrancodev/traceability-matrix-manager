d68309ae8773bcba33a57ccdfab997d66f9a5be0
Omar Franco <fofe2803@gmail.com>
2023-12-04 18:46:54 -0600

    feat(user): customize the create_user, roles and requeried fields
    customize the create_user methods to avoid username as requeired fields, remove some useless fields, add field role, and migrate the changes

--- traceabilitymatrix/user/models.py
+++ traceabilitymatrix/user/models.py
@@ -1,10 +1,54 @@
-from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+
+
+class UserManager(BaseUserManager):
+
+    def _create_user(self, email, password=None, **extra_fields):
+        """Create and save a User with the given email and password."""
+        if not email:
+            raise ValueError("The given email must be set")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        user = self._create_user(email, password, **extra_fields)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self._create_user(email, password, **extra_fields)
+
 
 class User(AbstractUser):
-    full_name = models.CharField(max_length=255)
-    email = models.EmailField(unique=True)
-    password = models.CharField(max_length=255)
-    is_admin = models.BooleanField(default=False)
-    is_guest = models.BooleanField(default=False)
-    assigned_projects = models.ManyToManyField('project.Project', related_name='%(class)s_assigned_users', blank=True)
+    class Role(models.TextChoices):
+        Admin = "Admin", "Admin"
+        TeamMember = "Team Member", "Team Member"
+        Guest = "Guest", "Guest"
+
+    role = models.CharField(max_length=50, choices=Role.choices)
+    fullName = models.CharField(max_length=255)
+    email = models.EmailField(unique=True, verbose_name="Email Address")
+    assignedProjects = models.ManyToManyField(
+        "project.Project", related_name="assignedUsers", blank=True
+    )
+    username = None
+    first_name = None
+    last_name = None
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["fullName","role", "username", "password"]
+
+    objects = UserManager()
