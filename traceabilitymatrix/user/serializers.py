35bcc05084260d2e1cfd67302d4676a1bd367589
Omar Franco <fofe2803@gmail.com>
2023-12-06 11:10:18 -0600

    feat(user): add filter, simplify update function and add other serializer
    add filter to get other projects with isDraft true, simplify update function and add other serializer to list users

--- traceabilitymatrix/user/serializers.py
+++ traceabilitymatrix/user/serializers.py
@@ -1,11 +1,17 @@
 from rest_framework import serializers
+from django.db.models import Q
 from djoser.serializers import UserCreateSerializer, UserSerializer
 from project.serializers import ProjectSerializer
 from project.models import Project
 
+
 class CustomUserCreateSerializer(UserCreateSerializer):
     class Meta(UserCreateSerializer.Meta):
         fields = ["id", "fullName", "email", "role", "password"]
+
+class CustomListUserSerializer(UserCreateSerializer):
+    class Meta(UserCreateSerializer.Meta):
+        fields = ["id", "fullName", "email", "role"]
 
 
 class CustomUserSerializer(UserSerializer):
@@ -16,19 +22,28 @@ class CustomUserSerializer(UserSerializer):
         allow_empty=True,
     )
     otherProjects = serializers.SerializerMethodField()
-    
+
     class Meta(UserSerializer.Meta):
-        fields = ["id", "fullName", "email", "role", "myProjects", "otherProjects", "assignedProjects"]
-    
+        fields = [
+            "id",
+            "fullName",
+            "email",
+            "role",
+            "myProjects",
+            "otherProjects",
+            "assignedProjects",
+        ]
+
     def get_otherProjects(self, instance):
         assignedProjectIds = instance.myProjects.values_list("id", flat=True)
-        other_projects = Project.objects.exclude(id__in=assignedProjectIds)
+        other_projects = Project.objects.filter(
+            ~Q(id__in=assignedProjectIds), isDraft=True
+        )
         return ProjectSerializer(other_projects, many=True).data
-    
+
     def update(self, instance, validated_data):
         assignedProjectIds = validated_data.pop("assignedProjects", [])
         instance = super().update(instance, validated_data)
-        if assignedProjectIds:
-            instance.myProjects.set(assignedProjectIds)
-        
+        instance.myProjects.set(assignedProjectIds)
+
         return instance
