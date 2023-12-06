55005ce773aa8723d6dc88f4a42c4ec539e4df80
Omar Franco <fofe2803@gmail.com>
2023-12-06 11:11:07 -0600

    feat(user): remove create groups logic, add a filter to restrict change roles

--- traceabilitymatrix/user/signals.py
+++ traceabilitymatrix/user/signals.py
@@ -1,55 +1,33 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import pre_save, post_save
 from django.dispatch import receiver
 from django.contrib.auth import get_user_model
-from django.contrib.auth.models import Group, Permission
+from django.contrib.auth.models import Group
+from rest_framework.exceptions import PermissionDenied
 
 User = get_user_model()
 
 
+@receiver(pre_save, sender=User)
+def restrict_role_change(sender, instance, **kwargs):
+    if instance.pk is not None:
+        originalInstance = User.objects.get(pk=instance.pk)
+        if originalInstance.role != instance.role:
+            response_data = {"message": "Changing user role is not allowed."}
+            raise PermissionDenied(response_data)
+
+
 @receiver(post_save, sender=User)
 def assign_permissions(sender, instance, created, **kwargs):
+    grp = " Group"
     if created:
         role = instance.role
 
         if role == User.Role.Admin:
-            group, created = Group.objects.get_or_create(name="Admin Group")
-            permissions = Permission.objects.filter(
-                codename__in=[
-                    "view_project",
-                    "add_project",
-                    "change_project",
-                    "delete_project",
-                    "add_record",
-                    "view_record",
-                    "change_record",
-                    "delete_record",
-                    "view_user",
-                    "add_user",
-                    "change_user",
-                    "delete_user",
-                ]
-            )
-            group.permissions.set(permissions)
+            group = Group.objects.get(name=User.Role.Admin + grp)
             instance.groups.add(group)
-
         elif role == User.Role.TeamMember:
-            group, created = Group.objects.get_or_create(name="Team Member Group")
-            permissions = Permission.objects.filter(
-                codename__in=[
-                    "view_project",
-                    "view_record",
-                    "change_project",
-                    "add_record",
-                    "change_record",
-                    "delete_record",
-                ]
-            )
-            group.permissions.set(permissions)
+            group = Group.objects.get(name=User.Role.TeamMember + grp)
             instance.groups.add(group)
         elif role == User.Role.Guest:
-            group, created = Group.objects.get_or_create(name="Guest Group")
-            permissions = Permission.objects.filter(
-                codename__in=["view_project", "view_record"]
-            )
-            group.permissions.set(permissions)
+            group = Group.objects.get(name=User.Role.Guest + grp)
             instance.groups.add(group)
