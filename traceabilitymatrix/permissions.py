bd504b9c9fc33e8e4728c092b629a2c615461f5c
Omar Franco <fofe2803@gmail.com>
2023-12-07 12:50:30 -0600

    fix: fix the check object permission function
    fix the filter on check object permission function depending on the type of object

--- traceabilitymatrix/permissions.py
+++ traceabilitymatrix/permissions.py
@@ -1,6 +1,9 @@
-from rest_framework.permissions import DjangoModelPermissions
 from rest_framework.permissions import BasePermission
 from django.contrib.auth import get_user_model
+from eventrecord.models import EventRecord
+
+from project.models import Project
+from record.models import Record
 
 User = get_user_model()
 grp = " Group";
@@ -14,11 +17,25 @@ class TeamMemberPermission(BasePermission):
         return request.user and request.user.groups.filter(name=User.Role.TeamMember + grp).exists()
     
     def has_object_permission(self, request, view, obj):
-        return obj.assignedUsers.filter(id=request.user.id).exists()
+        if isinstance(obj, Project):
+            return obj.assignedUsers.filter(id=request.user.id).exists()
+        elif isinstance(obj, Record):
+            project = obj.associatedProject
+            return project.assignedUsers.filter(id=request.user.id).exists()
+        elif isinstance(obj, EventRecord):
+            return True
+        return False
     
 class GuestPermission(BasePermission):
     def has_permission(self, request, view):
         return request.user and request.user.groups.filter(name=User.Role.Guest + grp).exists()
         
     def has_object_permission(self, request, view, obj):
-        return obj.assignedUsers.filter(id=request.user.id).exists()
+        if isinstance(obj, Project):
+            return obj.assignedUsers.filter(id=request.user.id).exists()
+        elif isinstance(obj, Record):
+            project = obj.associatedProject
+            return project.assignedUsers.filter(id=request.user.id).exists()
+        elif isinstance(obj, EventRecord):
+            return True
+        return False
