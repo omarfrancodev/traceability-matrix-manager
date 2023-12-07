05c00068b82305def553d850dedfbacbe332432a
Omar Franco <fofe2803@gmail.com>
2023-12-07 00:52:51 -0600

    feat: add and change signals
    add and change signals to validate role change, assign permission and create event record on delete user and update roles

--- user/signals.py
+++ user/signals.py
@@ -1,33 +1,72 @@
-from django.db.models.signals import pre_save, post_save
+from django.db.models.signals import pre_save, post_save, pre_delete
 from django.dispatch import receiver
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import Group
 from rest_framework.exceptions import PermissionDenied
+from django_currentuser.middleware import get_current_authenticated_user
+from eventrecord.models import EventRecord
 
 User = get_user_model()
+
+
+@receiver(pre_delete, sender=User)
+def user_deleted(sender, instance, **kwargs):
+    current_user = get_current_authenticated_user()
+    EventRecord.objects.create(
+        actionType=EventRecord.Action.Delete,
+        userFullNameExec=current_user.fullName,
+        userFullNameAffected=instance.fullName,
+        userRoleExec=current_user.role,
+        userRoleAffected=instance.role,
+        appModel=User.__name__,
+    )
 
 
 @receiver(pre_save, sender=User)
 def restrict_role_change(sender, instance, **kwargs):
     if instance.pk is not None:
-        originalInstance = User.objects.get(pk=instance.pk)
-        if originalInstance.role != instance.role:
-            response_data = {"message": "Changing user role is not allowed."}
-            raise PermissionDenied(response_data)
+        original_instance = User.objects.get(pk=instance.pk)
+        current_user = get_current_authenticated_user()
+
+        if original_instance.id != current_user.id:
+            update_role_create_event_record(current_user, instance)
+        elif original_instance.role != instance.role:
+            if original_instance.role != User.Role.Admin:
+                response_data = {
+                    "message": "You do not have permission to perform this action. Changing the user role is not allowed for non-admin users."
+                }
+                raise PermissionDenied(response_data)
+            update_role_create_event_record(original_instance, instance)
+
+
+def update_role_create_event_record(original_instance, instance):
+    instance.groups.clear()
+    assign_groups(instance, instance.role)
+    EventRecord.objects.create(
+        actionType=EventRecord.Action.EditRole,
+        userFullNameExec=original_instance.fullName,
+        userFullNameAffected=instance.fullName,
+        userRoleExec=original_instance.role,
+        userRoleAffected=instance.role,
+        appModel=User.__name__,
+    )
 
 
 @receiver(post_save, sender=User)
 def assign_permissions(sender, instance, created, **kwargs):
-    grp = " Group"
     if created:
         role = instance.role
+        assign_groups(instance, role)
 
-        if role == User.Role.Admin:
-            group = Group.objects.get(name=User.Role.Admin + grp)
-            instance.groups.add(group)
-        elif role == User.Role.TeamMember:
-            group = Group.objects.get(name=User.Role.TeamMember + grp)
-            instance.groups.add(group)
-        elif role == User.Role.Guest:
-            group = Group.objects.get(name=User.Role.Guest + grp)
-            instance.groups.add(group)
+
+def assign_groups(instance, role):
+    grp = " Group"
+    if role == User.Role.Admin:
+        group = Group.objects.get(name=User.Role.Admin + grp)
+        instance.groups.add(group)
+    elif role == User.Role.TeamMember:
+        group = Group.objects.get(name=User.Role.TeamMember + grp)
+        instance.groups.add(group)
+    elif role == User.Role.Guest:
+        group = Group.objects.get(name=User.Role.Guest + grp)
+        instance.groups.add(group)
