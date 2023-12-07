7bdc69c1560b44e6161bbd8393638c21c2ae35b1
Omar Franco <fofe2803@gmail.com>
2023-12-07 13:43:23 -0600

    style(project): formatting the file, change records representation
    formatting the file, change records representation to sorted them by id

--- project/serializers.py
+++ project/serializers.py
@@ -2,31 +2,50 @@ from rest_framework import serializers
 from .models import Project
 from record.serializers import RecordSerializer
 
+
 class ProjectSerializer(serializers.ModelSerializer):
     owner = serializers.SlugRelatedField(read_only=True, slug_field="fullName")
-    
+
     class Meta:
         model = Project
-        fields = ['id', 'name', 'description', 'isPublished', 'owner']
-        
+        fields = ["id", "name", "description", "isPublished", "owner"]
+
+
 class DetailProjectSerializer(serializers.ModelSerializer):
     owner = serializers.SlugRelatedField(read_only=True, slug_field="fullName")
-    
+
     class Meta:
         model = Project
-        fields = ['id', 'name', 'description', 'owner', 'isPublished', 'createdAt', 'updatedAt']
+        fields = [
+            "id",
+            "name",
+            "description",
+            "owner",
+            "isPublished",
+            "createdAt",
+            "updatedAt",
+        ]
+
 
 class DetailProjectRecordSerializer(serializers.ModelSerializer):
     associatedRecords = RecordSerializer(many=True, read_only=True)
 
     class Meta:
         model = Project
-        fields = ['id', 'name', 'associatedRecords']
+        fields = ["id", "name", "associatedRecords"]
+    
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        data['associatedRecords'] = sorted(data['associatedRecords'], key=lambda x: x['id'])
+        return data
+
 
 class DetailUsersProjectSerializer(serializers.ModelSerializer):
-    assignedUsers = serializers.SlugRelatedField(many=True, read_only=True, slug_field="fullName")
+    assignedUsers = serializers.SlugRelatedField(
+        many=True, read_only=True, slug_field="fullName"
+    )
     owner = serializers.SlugRelatedField(read_only=True, slug_field="fullName")
 
     class Meta:
         model = Project
-        fields = ['id', 'name', 'owner', 'assignedUsers']
+        fields = ["id", "name", "owner", "assignedUsers"]
