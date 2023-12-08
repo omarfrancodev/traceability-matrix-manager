cd3d703cd835ab91d692182fc2554bd3b52b78f1
Omar Franco <fofe2803@gmail.com>
2023-12-08 02:57:02 -0600

    feat(project): add a signal when created project
    add a signal to automatically assign the current user (Admin) to the created project

--- project/signals.py
+++ project/signals.py
@@ -1,8 +1,12 @@
-from django.db.models.signals import pre_delete
+from django.db.models.signals import pre_delete, post_save
 from django.dispatch import receiver
 from eventrecord.models import EventRecord
 from django_currentuser.middleware import get_current_authenticated_user
 from .models import Project
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
 
 @receiver(pre_delete, sender=Project)
 def project_deleted(sender, instance, **kwargs):
@@ -13,3 +17,13 @@ def project_deleted(sender, instance, **kwargs):
         userRoleExec=current_user.role,
         appModel=Project.__name__,
     )
+
+
+@receiver(post_save, sender=Project)
+def assign_project_to_user(sender, instance, created, **kwargs):
+    if created:
+        current_user = get_current_authenticated_user()
+        assignedProjectIds = list(current_user.myProjects.values_list("id", flat=True))
+        assignedProjectIds.append(instance.id)
+        current_user.myProjects.set(assignedProjectIds)
+        current_user.save()
