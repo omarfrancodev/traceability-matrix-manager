bd0f78cb1988ff267f802a0212818efb9d7d957b
Omar Franco <fofe2803@gmail.com>
2023-12-10 14:04:30 -0600

    feat(event record): add a custom response to view, and change imports of permission classes

--- eventrecord/views.py
+++ eventrecord/views.py
@@ -1,11 +1,25 @@
-from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework import generics, status
 from django.utils import timezone
 from .models import EventRecord
 from .serializers import EventRecordSerializer
 from rest_framework.permissions import DjangoModelPermissions
-from traceabilitymatrix.permissions import AdminPermission, TeamMemberPermission, GuestPermission
+from custompermissions.permissions import AdminPermission, TeamMemberPermission, GuestPermission
 
 class EventRecordListView(generics.ListCreateAPIView):
     queryset = EventRecord.objects.filter(clientLocalTimestamp__gte=timezone.now() - timezone.timedelta(days=3)).order_by('-serverTimestamp')
     serializer_class = EventRecordSerializer
     permission_classes = [DjangoModelPermissions, (AdminPermission | TeamMemberPermission | GuestPermission)]
+    
+    def list(self, request, *args, **kwargs):
+        try:
+            response = super().list(request, *args, **kwargs)
+            return Response(
+                data={'message': 'Event Records retrieved successfully', 'data': response.data},
+                status=status.HTTP_200_OK
+            )
+        except Exception as e:
+            return Response(
+                data={'message': f'Error retrieving users: {str(e)}'},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
