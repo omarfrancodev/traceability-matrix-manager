b3217fc9e530aeb56c0bb06e80fb516998872d5b
Omar Franco <fofe2803@gmail.com>
2023-12-11 01:40:21 -0600

    refactor(record): change detail serializer to impeding get action

--- record/views.py
+++ record/views.py
@@ -1,14 +1,23 @@
 from rest_framework import generics, status
 from rest_framework.response import Response
 from .models import Record
+from rest_framework.exceptions import PermissionDenied
 from .serializers import RecordSerializer, RecordDetailSerializer
 from rest_framework.permissions import DjangoModelPermissions
-from custompermissions.permissions import AdminPermission, TeamMemberPermission, GuestPermission
+from custompermissions.permissions import (
+    AdminPermission,
+    TeamMemberPermission,
+    GuestPermission,
+)
+
 
 class RecordListView(generics.CreateAPIView):
     queryset = Record.objects.all()
     serializer_class = RecordSerializer
-    permission_classes = [DjangoModelPermissions, (AdminPermission | TeamMemberPermission | GuestPermission)]
+    permission_classes = [
+        DjangoModelPermissions,
+        (AdminPermission | TeamMemberPermission | GuestPermission),
+    ]
 
     def create(self, request, *args, **kwargs):
         try:
@@ -18,33 +27,35 @@ class RecordListView(generics.CreateAPIView):
             self.perform_create(serializer)
             headers = self.get_success_headers(serializer.data)
             return Response(
-                data={'message': 'Record created successfully', 'data': serializer.data},
+                data={
+                    "message": "Record created successfully",
+                    "data": serializer.data,
+                },
                 status=status.HTTP_201_CREATED,
-                headers=headers
+                headers=headers,
             )
         except Exception as e:
             return Response(
-                data={'message': f'Error creating record: {str(e)}'},
-                status=status.HTTP_400_BAD_REQUEST
+                data={"message": f"Error creating record: {str(e)}"},
+                status=status.HTTP_400_BAD_REQUEST,
             )
+
 
 class RecordDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Record.objects.all()
     serializer_class = RecordSerializer
-    permission_classes = [DjangoModelPermissions, (AdminPermission | TeamMemberPermission | GuestPermission)]
+    permission_classes = [
+        DjangoModelPermissions,
+        (AdminPermission | TeamMemberPermission | GuestPermission),
+    ]
 
     def retrieve(self, request, *args, **kwargs):
-        try:
-            response = super().retrieve(request, *args, **kwargs)
-            return Response(
-                data={'message': 'Record retrieved successfully', 'data': response.data},
-                status=status.HTTP_200_OK
-            )
-        except Exception as e:
-            return Response(
-                data={'message': f'Error retrieving record: {str(e)}'},
-                status=status.HTTP_404_NOT_FOUND
-            )
+        raise PermissionDenied(
+            {
+                "message": "You do not have permission to perform this action. You are not allowed to get record with this endpoint. Use /records/{id}/tracing instead"
+            },
+            status.HTTP_403_FORBIDDEN,
+        )
 
     def update(self, request, *args, **kwargs):
         try:
@@ -52,20 +63,23 @@ class RecordDetailView(generics.RetrieveUpdateDestroyAPIView):
                 instance = self.get_object()
             except Exception as e:
                 return Response(
-                    data={'message': f'Error finding record: {str(e)}'},
-                    status=status.HTTP_404_NOT_FOUND
+                    data={"message": f"Error finding record: {str(e)}"},
+                    status=status.HTTP_404_NOT_FOUND,
                 )
             serializer = self.get_serializer(instance, data=request.data, partial=True)
             serializer.is_valid(raise_exception=True)
             self.perform_update(serializer)
             return Response(
-                data={'message': 'Record updated successfully', 'data': serializer.data},
-                status=status.HTTP_200_OK
+                data={
+                    "message": "Record updated successfully",
+                    "data": serializer.data,
+                },
+                status=status.HTTP_200_OK,
             )
         except Exception as e:
             return Response(
-                data={'message': f'Error updating record: {str(e)}'},
-                status=status.HTTP_400_BAD_REQUEST
+                data={"message": f"Error updating record: {str(e)}"},
+                status=status.HTTP_400_BAD_REQUEST,
             )
 
     def destroy(self, request, *args, **kwargs):
@@ -74,34 +88,41 @@ class RecordDetailView(generics.RetrieveUpdateDestroyAPIView):
                 instance = self.get_object()
             except Exception as e:
                 return Response(
-                    data={'message': f'Error finding record: {str(e)}'},
-                    status=status.HTTP_404_NOT_FOUND
+                    data={"message": f"Error finding record: {str(e)}"},
+                    status=status.HTTP_404_NOT_FOUND,
                 )
             self.perform_destroy(instance)
             return Response(
-                data={'message': 'Record deleted successfully'},
-                status=status.HTTP_204_NO_CONTENT
+                data={"message": "Record deleted successfully"},
+                status=status.HTTP_204_NO_CONTENT,
             )
         except Exception as e:
             return Response(
-                data={'message': f'Error deleting record: {str(e)}'},
-                status=status.HTTP_404_NOT_FOUND
+                data={"message": f"Error deleting record: {str(e)}"},
+                status=status.HTTP_404_NOT_FOUND,
             )
+
 
 class RecordDetailTraceView(generics.RetrieveAPIView):
     queryset = Record.objects.all()
     serializer_class = RecordDetailSerializer
-    permission_classes = [DjangoModelPermissions, (AdminPermission | TeamMemberPermission | GuestPermission)]
+    permission_classes = [
+        DjangoModelPermissions,
+        (AdminPermission | TeamMemberPermission | GuestPermission),
+    ]
 
     def retrieve(self, request, *args, **kwargs):
         try:
             response = super().retrieve(request, *args, **kwargs)
             return Response(
-                data={'message': 'Record retrieved successfully', 'data': response.data},
-                status=status.HTTP_200_OK
+                data={
+                    "message": "Record retrieved successfully",
+                    "data": response.data,
+                },
+                status=status.HTTP_200_OK,
             )
         except Exception as e:
             return Response(
-                data={'message': f'Error retrieving record: {str(e)}'},
-                status=status.HTTP_404_NOT_FOUND
+                data={"message": f"Error retrieving record: {str(e)}"},
+                status=status.HTTP_404_NOT_FOUND,
             )
