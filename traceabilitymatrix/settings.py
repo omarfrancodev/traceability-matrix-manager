0b289111a6122194ab7c1d41a5a430d0b1e377ec
Omar Franco <fofe2803@gmail.com>
2023-12-11 11:49:13 -0600

    refactor(traceabilitymatrix): change the jwt token lifetime settings

--- traceabilitymatrix/settings.py
+++ traceabilitymatrix/settings.py
@@ -110,10 +110,11 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
+    
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(hours=12),
+    "ACCESS_TOKEN_LIFETIME": timedelta(hours=1),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "AUTH_HEADER_TYPES": ("Bearer",),
 }
